016b577db2212a764971674395d7d402d5fdecef
m0r13 <moritz.hilscher@gmail.com>
2017-01-08 22:31:57 +0100

    Building project using platformio.

---
+++ src/wificonfig.h
@@ -0,0 +1,12 @@
+//#define WIFI_AP
+#ifdef WIFI_AP
+# define WIFI_SSID "HotspotForAHotbox"
+# define WIFI_PWD "nebel-in-the-cloud"
+#else
+//# define WIFI_SSID "53301330"
+//# define WIFI_PWD "ca4c1af203c545cb20c2aa740aa46cb123a46b438d5601721ab037823dd5dba7"
+//# define WIFI_SSID "XGME Hotspotbox"
+//# define WIFI_PWD ""
+# define WIFI_SSID "HotspotForAHotbox"
+# define WIFI_PWD "nebel-in-the-cloud"
+#endif
